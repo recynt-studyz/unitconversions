97090def5c1309729bcf954a17baa58551b66900
Brennan Kollasch <201308643+recynt-studyz@users.noreply.github.com>
2026-07-14 12:32:37 -0700

    add Google Analytics 4 G-6YZPEXQKHV

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,6 +71,18 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-6YZPEXQKHV"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6YZPEXQKHV');
+          `}
+        </Script>
       </body>
     </html>
   )
